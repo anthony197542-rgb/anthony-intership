9fb30fa8b494cbbd5cea1cd5b63261281542a298
Anthony Tancredi <anthony197542@gmail.com>
2026-07-23 18:23:38 +0000

    hotcollections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
 
 const HotCollections = () => {
+  const [collections, setCollections] =useState([]);
+
+
+  useEffect(() => {
+  const fetchCollections = async () => {
+    const {data} = await axios.get(
+      "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+    );
+
+
+      console.log(data);
+      setCollections(data);
+  };
+
+  fetchCollections();
+}, []);
+
+  
+
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,7 +35,7 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {collections.map((collection, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
@@ -30,7 +51,7 @@ const HotCollections = () => {
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>collection.title</h4>
                   </Link>
                   <span>ERC-192</span>
                 </div>
