4389814e52792b9c8af55bb607d9b1f2eda77ef1
Anthony Tancredi <anthony197542@gmail.com>
2026-07-25 23:04:59 +0000

    Restore React hook imports

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
