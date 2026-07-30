2467257b2cb56de3605ad52c76ffe53199eff2b7
Your Name <anthony197542@gmail.com>
2026-07-30 16:04:07 -0400

    update TopSellers

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,89 +1,96 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Skeleton from "../UI/Skeleton"; 
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
-const [sellers, setSellers] = useState([]); 
-const [loading, setLoading] = useState(true)
+  const [sellers, setSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-useEffect (() => {
-  async function fetchTopSellers() {
-    try {
-      const { data } = await axios.get(
-        "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+  // Click handler
+  const handleSellerClick = (seller) => {
+    console.log("Seller clicked:", seller);
+  };
 
-      );
-      setSellers(data);
-    } catch (error) {
-      console.error("Error fetching top sellers:", error);
-    } finally {
-      setLoading(false);
-
+  useEffect(() => {
+    async function fetchTopSellers() {
+      try {
+        const { data } = await axios.get(
+             "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+        );
+          console.log(data[0]);
+        setSellers(data);
+      } catch (error) {
+      
+      } finally {
+        setLoading(false);
+      }
     }
-  }
-fetchTopSellers();
-},[]);
 
-return (
-  <section id="section-popular" className="pb-5">
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="text-center">
+    fetchTopSellers();
+  }, []);
+
+  return (
+    <section id="section-popular" className="pb-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-center">
             <h2>Top Sellers</h2>
-             <div className="small-border bg-color-2"></div>
-             </div>
-             </div>
-             <div className="col-md-12">
-              <ol className="author_list">
-                {loading
-                ? new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Skeleton
-                    width="50px"
-                    height="50"
-                    borderRadius="50%"
-                    />
-                    </div>
-                    <div className="author_list_info">
-                    <Skeleton width="100px" height="20px" />
-                    <Skeleton width="40px" height="15px" />
-                    </div>
-                    </li>
-                    ))
-                   : sellers.map((seller) => (
-                    <li key={seller.id}>
+            <div className="small-border bg-color-2"></div>
+          </div>
+
+          <div className="col-md-12">
+            <ol className="author_list">
+              {loading
+                ? Array.from({ length: 12 }).map((_, index) => (
+                    <li key={index}>
                       <div className="author_list_pp">
-                      <Link to={`/author/${seller.authorId}`}>
-                        <img
-                        className="lazy pp-author"
-                        src={seller.authorImage}
-                        alt={seller.authorName}
+                        <Skeleton
+                          width="50px"
+                          height="50px"
+                          borderRadius="50%"
                         />
-                        <i className="fa fa-check"></i>
+                      </div>
+                      <div className="author_list_info">
+                        <Skeleton width="100px" height="20px" />
+                        <Skeleton width="40px" height="15px" />
+                      </div>
+                    </li>
+                  ))
+                : sellers.map((seller) => (
+                    <li
+                      key={seller.id}
+                      onClick={() => handleSellerClick(seller)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <div className="author_list_pp">
+                        <Link to={`/author/${seller.authorId}`}>
+                          <img
+                            className="lazy pp-author"
+                            src={seller.authorImage}
+                            alt={seller.authorName}
+                          />
+                          <i className="fa fa-check"></i>
                         </Link>
-                        </div>
-                        <div className="author_list_info">
-                          <Link to={`/author/${seller.authorId}`}>
+                      </div>
+
+                      <div className="author_list_info">
+                        <Link to={`/author/${seller.authorId}`}>
                           {seller.authorName}
-                          </Link>
-                          <span>{seller.price} ETH</span>
-                          </div>
-                          </li>
-                    ))}
+                        </Link>
+                        <span>{seller.total.price} ETH</span>
+                      </div>
+                    </li>
+                  ))}
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-                    </ol>
-                    </div>
-                    </div>
-                    </div>
-                    </section>
-                  );
-                  };
+export default TopSellers;
 
-
-                  export default TopSellers;
-                    
                     
             
